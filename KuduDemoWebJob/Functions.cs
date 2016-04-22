1f1ba1be71422e4a270b06ebc663a8d6d491b73f
Keisha Waithe <keisha_waithe@hotmail.com>
2016-04-22 03:00:45 -0400

    with webjob

---
+++ KuduDemoWebJob/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace KuduDemoWebJob
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([TimerTrigger("00:00:10", RunOnStartup = true)] TimerInfo timerInfo)
+        {
+            Console.WriteLine("Just doing my job!");
+        }
+    }
+}
